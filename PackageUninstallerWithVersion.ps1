73a214edd73ab86b11c50d6c472dcbf7828311eb
Salvia <SalviaSage@users.noreply.github.com>
2020-04-10 11:56:11 -0400

    Update PackageUninstallerWithVersion.ps1

--- PackageUninstallerWithVersion.ps1
+++ PackageUninstallerWithVersion.ps1
@@ -1,4 +1,4 @@
-## This will uninstall a specific version of a specific module
+## This will uninstall a specific version of a specific module. Just replace the string in the quotes with the name of the module and the version of the module.
 ## It is also possible to check if a module is instaled or to check for a specific version of a module using commands.
 
 Get-Package -Name "DSCAccelerator" -RequiredVersion "2.1" | Uninstall-Package
